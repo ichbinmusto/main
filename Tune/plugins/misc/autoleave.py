87f8ab297c56ad1cdbee9d2b1c495a965712bf8b
Certified Coder <155803358+CertifiedCoders@users.noreply.github.com>
2025-04-08 22:32:45 +0530

    Update autoleave.py

--- Tune/plugins/misc/autoleave.py
+++ Tune/plugins/misc/autoleave.py
@@ -10,7 +10,38 @@ from Tune.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
-    pass  # This function does nothing, effectively disabling the auto leave assistant feature
+    if config.AUTO_LEAVING_ASSISTANT:
+        while not await asyncio.sleep(900):
+            from Tune.core.userbot import assistants
+
+            for num in assistants:
+                client = await get_client(num)
+                left = 0
+                try:
+                    async for i in client.get_dialogs():
+                        if i.chat.type in [
+                            ChatType.SUPERGROUP,
+                            ChatType.GROUP,
+                            ChatType.CHANNEL,
+                        ]:
+                            if (
+                                i.chat.id != config.LOGGER_ID
+                                and i.chat.id != -1001686672798
+                                and i.chat.id != -1001549206010
+                            ):
+                                if left == 20:
+                                    continue
+                                if not await is_active_chat(i.chat.id):
+                                    try:
+                                        await client.leave_chat(i.chat.id)
+                                        left += 1
+                                    except:
+                                        continue
+                except:
+                    pass
+
+
+asyncio.create_task(auto_leave())
 
 
 async def auto_end():
@@ -40,5 +71,4 @@ async def auto_end():
                     continue
 
 
-asyncio.create_task(auto_leave())
 asyncio.create_task(auto_end())
