2a5e0c54d627ec53828e42468467dcc85bd70ce0
Certified Coder <155803358+CertifiedCoders@users.noreply.github.com>
2025-04-09 15:30:32 +0530

    owo

--- Tune/plugins/misc/watcher.py
+++ Tune/plugins/misc/watcher.py
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Jarvis.stop_stream_force(message.chat.id)
+    await Jarvis.force_stop_stream(message.chat.id)
--- Tune/plugins/tools/reload.py
+++ Tune/plugins/tools/reload.py
@@ -53,7 +53,7 @@ async def restartbot(client, message: Message, _):
     await asyncio.sleep(1)
     try:
         db[message.chat.id] = []
-        await Jarvis.stop_stream_force(message.chat.id)
+        await Jarvis.force_stop_stream(message.chat.id)
     except:
         pass
     userbot = await get_assistant(message.chat.id)
@@ -80,7 +80,7 @@ async def restartbot(client, message: Message, _):
             pass
         try:
             db[chat_id] = []
-            await Jarvis.stop_stream_force(chat_id)
+            await Jarvis.force_stop_stream(chat_id)
         except:
             pass
     return await mystic.edit_text(_["reload_5"].format(app.mention))
