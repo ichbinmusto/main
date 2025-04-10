2a580fc8d16da2a0ac9630dc2418e9de42ddfbb8
Certified Coder <155803358+CertifiedCoders@users.noreply.github.com>
2025-04-11 00:29:43 +0530

    🤌

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ OWNER_ID = int(getenv("OWNER_ID", 7044783841))
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github/CertifiedCoders/TuneViaBot/")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github/CertifiedCoders/TuneViaBot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN", '')
 
