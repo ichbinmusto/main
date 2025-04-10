fcb1efdf7d0c280d2e99dcd83b5f00d8c720195e
Certified Coder <155803358+CertifiedCoders@users.noreply.github.com>
2025-04-10 18:46:20 +0530

    Update speed.py

--- Tune/plugins/admins/speed.py
+++ Tune/plugins/admins/speed.py
@@ -38,7 +38,7 @@ async def playback(cli, message: Message, _, chat_id):
 
 @app.on_callback_query(filters.regex("SpeedUP") & ~BANNED_USERS)
 @languageCB
-async def del_back_playlist(client, CallbackQuery, _):
+async def manage_callback(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     chat, speed = callback_request.split("|")
