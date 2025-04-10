1b430eeb05ca708d7fbdf8768566b1831765bef2
Certified Coder <155803358+CertifiedCoders@users.noreply.github.com>
2025-04-10 10:20:32 +0530

    ok ✅

--- Tune/utils/admin_filters.py
+++ Tune/utils/admin_filters.py
@@ -3,41 +3,20 @@ from Tune.utils.admin_check import admin_check
 
 USE_AS_BOT = True
 
-def f_sudo_filter(filt, client, message):
-    return bool(
-        (
-            (message.from_user and message.from_user.id in SUDO_USERS)
-            or (message.sender_chat and message.sender_chat.id in SUDO_USERS)
-        )
-        and
-        not message.edit_date
-    )
-
-sudo_filter = filters.create(func=f_sudo_filter, name="SudoFilter")
-
-
-def onw_filter(filt, client, message):
+def owner_filter_func(filt, client, message):
     if USE_AS_BOT:
-        return bool(
-            True
-            and
-            not message.edit_date
-        )
+        return not message.edit_date
     else:
         return bool(
             message.from_user
             and message.from_user.is_self
-            and
-            not message.edit_date
+            and not message.edit_date
         )
 
-f_onw_fliter = filters.create(func=onw_filter, name="OnwFilter")
+owner_filter = filters.create(func=owner_filter_func, name="OwnerFilter")
 
 
 async def admin_filter_f(filt, client, message):
-    return (
-        not message.edit_date
-        and await admin_check(message)
-    )
+    return not message.edit_date and await admin_check(message)
 
 admin_filter = filters.create(func=admin_filter_f, name="AdminFilter")
